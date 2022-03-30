4b0ef762162c7701a9c07b4f6ada84e28d5325ee
Florent Chuffart <florent.chuffart@gmail.com>
2022-03-30 12:37:07 +0200

    update 01 and 02 vignettes

---
+++ results/common.R
@@ -0,0 +1,1 @@
+if (!exists("mreadRDS")) { mreadRDS = memoise::memoise(readRDS) }
---
+++ results/default_params.R
@@ -0,0 +1,4 @@
+if (!exists("gse")) {gse = "GSE41037"}
+if (!exists("y_key")) {y_key = "age"}
+if (!exists("covs")) {covs = c("gender", "diseasestatus")}
+
