4e5a045d17f7685e3d75bd067646653d25afa6f3
FOXE38 <fabienjossaud@gmail.com>
2022-04-05 15:41:24 +0200

    Creation of AMAR vignettes and params' modif

--- results/params_GSE41037.R
+++ results/params_GSE41037.R
@@ -2,4 +2,5 @@ if (!exists("gse")) {gse = "GSE41037"}
 if (!exists("y_key")) {y_key = "age"}
 if (!exists("covariates")) {covariates = c("gender", "diseasestatus")}
 if (!exists("sample_blacklist")) {sample_blacklist = c("GSM1007327")}
+if (!exists("AMAR_covariates")) {covariates = c("gender", "diseasestatus")}
 
--- results/params_default.R
+++ results/params_default.R
@@ -2,4 +2,4 @@ if (!exists("gse")) {gse = "GSE41037"}
 if (!exists("y_key")) {y_key = "age"}
 if (!exists("covariates")) {covariates = c("gender", "diseasestatus")}
 if (!exists("sample_blacklist")) {sample_blacklist = c("GSM1007327")}
-if (!exists("AMAR_covariates")) {AMAR_covariates = c("gender", "diseasestatus")}
+if (!exists("AMAR_covariates")) {AMAR_covariates = c("gender")}
