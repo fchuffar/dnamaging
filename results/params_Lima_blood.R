e2caed330bb79b939e49bc5e745e0275ac5887f8
FOXE38 <fabienjossaud@gmail.com>
2022-05-01 20:21:28 +0200

    Params for Lima dataset Blood/Saliva tissue

---
+++ results/params_Lima_blood.R
@@ -0,0 +1,9 @@
+if (!exists("gse")) {gse = "exp5300_meth_blood_convoluted"}
+if (!exists("y_key")) {y_key = "Age"}
+if (!exists("covariates")) {covariates = c("Sexe", "cmslimapuno")}
+if (!exists("AMAR_covariates")) {AMAR_covariates = "cmslimapuno"}
+if (!exists("Lima")){ 
+  df_filename = paste0("study_", gse, ".rds")
+  Lima <- readRDS(df_filename)
+  Lima = cbind(Lima$exp_grp, t(Lima$data))
+}
