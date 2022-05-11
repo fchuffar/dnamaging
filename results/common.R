84f0570dd4e9ddc50e1afd45ea06ac8171a3b09e
Florent Chuffart <florent.chuffart@gmail.com>
2022-05-11 15:12:09 +0200

    Refactor and clarify CV part

--- results/common.R
+++ results/common.R
@@ -1,1 +1,7 @@
 if (!exists("mreadRDS")) { mreadRDS = memoise::memoise(readRDS) }
+
+if (!exists("cl")) {
+  nb_cores = parallel::detectCores()
+  cl <<- parallel::makeCluster(nb_cores,  type="FORK")
+  # parallel::stopCluster(cl)
+}
