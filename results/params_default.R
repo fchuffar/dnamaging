6a84500ea24196c97fc0b20a0097862a26b69288
Florent Chuffart <florent.chuffart@gmail.com>
2022-05-17 16:09:48 +0200

    cleaning params

--- results/params_default.R
+++ results/params_default.R
@@ -2,5 +2,4 @@ if (!exists("gse")) gse = "GSE41037"
 y_key = "age"
 covariates = c("gender", "diseasestatus")
 sample_blacklist = c("GSM1007327")
-AMAR_covariates = c("gender")
 if (file.exists(paste0("params_", gse, ".R"))) source(paste0("params_", gse, ".R")) 
