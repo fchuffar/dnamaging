ec4713a1e4945569b4075cd62202be6e30b3ddac
Florent Chuffart <florent.chuffart@gmail.com>
2022-05-19 13:37:24 +0200

    Fixing nb_train = 482

--- results/params_default.R
+++ results/params_default.R
@@ -2,5 +2,5 @@ if (!exists("gse")) gse = "GSE41037"
 y_key = "age"
 covariates = c("gender")
 sample_blacklist = c("GSM1007327")
-if (file.exists(paste0("params_", gse, ".R"))) source(paste0("params_", gse, ".R")) 
 nb_train = 482
+if (file.exists(paste0("params_", gse, ".R"))) source(paste0("params_", gse, ".R")) 
