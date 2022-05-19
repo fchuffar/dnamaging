45a443179dedd4c327b2a7f2e34eafec72ffe181
Florent Chuffart <florent.chuffart@gmail.com>
2022-05-19 13:28:49 +0200

    Fixing bug for nb_train = 482

--- results/params_default.R
+++ results/params_default.R
@@ -1,5 +1,6 @@
 if (!exists("gse")) gse = "GSE41037"
 y_key = "age"
-covariates = c("gender", "diseasestatus")
+covariates = c("gender")
 sample_blacklist = c("GSM1007327")
 if (file.exists(paste0("params_", gse, ".R"))) source(paste0("params_", gse, ".R")) 
+nb_train = 482
