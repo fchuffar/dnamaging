2362240da88dd92a32ecb933d08f4b7b3d5cc41e
FOXE38 <fabienjossaud@gmail.com>
2022-05-20 11:05:49 +0200

    Minor Modif

--- results/ci.R
+++ results/ci.R
@@ -1,5 +1,5 @@
 gses = c( 
-  # "GSE41037", # 27k Genome wide DNA methylation profiling of whole blood in schizophrenia patients and healthy subjects.
+  #"GSE41037", # 27k Genome wide DNA methylation profiling of whole blood in schizophrenia patients and healthy subjects.
   "GSE40279", # 450k Hannum 2013
   # "GSE20067", # 27k Genome wide DNA methylation profiling of diabetic nephropathy in type 1 diabetes mellitus
   # "GSE41169",
@@ -20,9 +20,9 @@ gses = c(
 for (gse in gses) {
   rm(list = ls()[-which(ls()%in%c("gse", "gses"))])
   n_boot = 500
-  n_boot = 300
+  #n_boot = 50
   print(paste0("************ ", gse, " ************"))
-  # rmarkdown::render("01_build_study_generic.Rmd", output_file=paste0("01_build_study_", gse, ".html"))
+  #rmarkdown::render("01_build_study_generic.Rmd", output_file=paste0("01_build_study_", gse, ".html"))
   rmarkdown::render("02_stats_desc.Rmd", output_file=paste0("02_stats_desc_", gse, ".html"))    
   rmarkdown::render("03_preproc.Rmd", output_file=paste0("03_preproc_", gse, ".html"))    
   rmarkdown::render("04_model.Rmd", output_file=paste0("04_model_", gse, ".html"))
