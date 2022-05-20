f561b83a10571633c08059a5009ee96b30cbc870
Florent Chuffart <florent.chuffart@gmail.com>
2022-05-20 16:03:24 +0200

    Fixing bug recursive loop in params_27kLima.R

--- results/params_27kLima.R
+++ results/params_27kLima.R
@@ -1,5 +1,4 @@
 y_key = "Age"
 covariates = c("citynocms", "citycms")
 sample_blacklist = c("GSM1007327")
-nb_train = 482
-if (file.exists(paste0("params_", gse, ".R"))) source(paste0("params_", gse, ".R")) 
+nb_train = 40
