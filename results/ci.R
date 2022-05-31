19a8b348243b4d9f43ffc2064cddc9c88ff5ce93
Florent Chuffart <florent.chuffart@gmail.com>
2022-05-31 17:03:11 +0200

    Evolv ci

--- results/ci.R
+++ results/ci.R
@@ -1,9 +1,31 @@
 gses = c( 
   "GSE41037", # 27k Genome wide DNA methylation profiling of whole blood in schizophrenia patients and healthy subjects.
-  # "GSE20067", # 27k Genome wide DNA methylation profiling of diabetic nephropathy in type 1 diabetes mellitus
+  "27kGSE40279", # Hannum 2013 27k version
+  "GSE40279", # 450k Hannum 2013
+  "cancair",
+  NULL
+)
+for (gse in gses) {
+  rm(list = ls()[-which(ls()%in%c("gse", "gses"))])
+  n_boot = 500
+  #n_boot = 50
+  print(paste0("************ ", gse, " ************"))
+  #rmarkdown::render("01_build_study_generic.Rmd", output_file=paste0("01_build_study_", gse, ".html"))
+  rmarkdown::render("02_stats_desc.Rmd", output_file=paste0("02_stats_desc_", gse, ".html"))    
+  rmarkdown::render("03_preproc.Rmd", output_file=paste0("03_preproc_", gse, ".html"))    
+  rmarkdown::render("04_model.Rmd", output_file=paste0("04_model_", gse, ".html"))
+  gse_m = gse ; rmarkdown::render("05_eval.Rmd", output_file=paste0("05_eval_", gse, ".html"))
+  # rmarkdown::render("06_amar.Rmd", output_file=paste0("06_amar_", gse, ".html"))
+}
+
+
+
+gses = c( 
+  "GSE41037", # 27k Genome wide DNA methylation profiling of whole blood in schizophrenia patients and healthy subjects.
+  "GSE20067", # 27k Genome wide DNA methylation profiling of diabetic nephropathy in type 1 diabetes mellitus
   "27kGSE40279", # Hannum 2013 27k version
   "27kLima",
-  "GSE40279", # 450k Hannum 2013
+  "Lima",
   # "GSE41169",
   # "GSE20236",
   # "GSE19711",
@@ -19,15 +41,14 @@ gses = c(
   # "GSE179759"
   NULL
 )
-for (gse in gses) {
+for (gse_new in gses) {
   rm(list = ls()[-which(ls()%in%c("gse", "gses"))])
+  gse_ref = "GSE40279"
+  gse_given = paste0(gse_ref, "g", gse_new)
+  rmarkdown::render("01_rebuild_study_generic.Rmd", output_file=paste0("01_rebuild_study_", gse_given, ".html")) # export df_{gse_given}.rds
   n_boot = 500
-  #n_boot = 50
-  print(paste0("************ ", gse, " ************"))
-  #rmarkdown::render("01_build_study_generic.Rmd", output_file=paste0("01_build_study_", gse, ".html"))
-  rmarkdown::render("02_stats_desc.Rmd", output_file=paste0("02_stats_desc_", gse, ".html"))    
-  rmarkdown::render("03_preproc.Rmd", output_file=paste0("03_preproc_", gse, ".html"))    
-  rmarkdown::render("04_model.Rmd", output_file=paste0("04_model_", gse, ".html"))
-  # rmarkdown::render("05_evaluation.Rmd", output_file=paste0("05_evaluation_", gse, ".html"))
-  # rmarkdown::render("06_amar.Rmd", output_file=paste0("06_amar_", gse, ".html"))
+  gse = gse_given ; rmarkdown::render("02_stats_desc.Rmd", output_file=paste0("02_stats_desc_", gse, ".html"))    
+  gse = gse_given ; rmarkdown::render("03_preproc.Rmd", output_file=paste0("03_preproc_", gse, ".html"))    
+  gse = gse_given ; rmarkdown::render("04_model.Rmd", output_file=paste0("04_model_", gse, ".html"))
+  gse_m = gse_given ; gse = gse_new ; rmarkdown::render("05_eval.Rmd", output_file=paste0("05_eval_", gse, ".html"))
 }
