bd54ba7100d258564e77948c00e74270661b78c6
Florent Chuffart <florent.chuffart@gmail.com>
2022-06-01 14:40:09 +0200

    Refactor litterature_models

--- results/ci.R
+++ results/ci.R
@@ -15,7 +15,6 @@ for (gse in gses) {
   rmarkdown::render("03_preproc.Rmd", output_file=paste0("03_preproc_", gse, ".html"))    
   rmarkdown::render("04_model.Rmd", output_file=paste0("04_model_", gse, ".html"))
   gse_m = gse ; rmarkdown::render("05_eval.Rmd", output_file=paste0("05_eval_", gse, ".html"))
-  # rmarkdown::render("06_amar.Rmd", output_file=paste0("06_amar_", gse, ".html"))
 }
 
 
--- results/common.R
+++ results/common.R
@@ -1,5 +1,4 @@
 if (!exists("mreadRDS")) { mreadRDS = memoise::memoise(readRDS, cache=cachem::cache_mem(max_size = 10*1024 * 1024^2)) }
-# if (!exists("mget_coefHannum")) mget_coefHannum = memoise::memoise(methylclockData::get_coefHannum)
 
 # services patterns
 if (!exists("mget_df_preproc")) {
