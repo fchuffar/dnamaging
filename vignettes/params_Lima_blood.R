4bd4b33ac1b9cad28653e7c5fee6cdca4f12367b
Florent Chuffart <florent.chuffart@gmail.com>
2022-06-14 11:43:39 +0200

    Delete params_Lima_blood.R

--- vignettes/params_Lima_blood.R
+++
@@ -1,4 +0,0 @@
-if (!exists("gse")) {gse = "exp5300_meth_blood_convoluted"}
-if (!exists("y_key")) {y_key = "Age"}
-if (!exists("covariates")) {covariates = c("Sexe", "cmslimapuno")}
-if (!exists("AMAR_covariates")) {AMAR_covariates = "cmslimapuno"}
