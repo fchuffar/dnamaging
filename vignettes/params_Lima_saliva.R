aede9ef484c0498fe8107a795e9ea42082feda30
Florent Chuffart <florent.chuffart@gmail.com>
2022-06-14 11:43:34 +0200

    Delete params_Lima_saliva.R

--- vignettes/params_Lima_saliva.R
+++
@@ -1,4 +0,0 @@
-if (!exists("gse")) {gse = "exp5300_meth_saliva_convoluted"}
-if (!exists("y_key")) {y_key = "Age"}
-if (!exists("covariates")) {covariates = c("Sexe", "cmslimapuno")}
-if (!exists("AMAR_covariates")) {AMAR_covariates = "cmslimapuno"}
