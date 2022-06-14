2c8c637accb45f4ace1701840c8e96fd935d6304
Florent Chuffart <florent.chuffart@gmail.com>
2022-06-14 11:43:44 +0200

    Delete build_df_Lima.R

--- vignettes/build_df_Lima.R
+++
@@ -1,5 +0,0 @@
-gse = "exp5300_meth_blood_convoluted"
-df_filename = paste0("study_", gse, ".rds")
-Lima = readRDS(df_filename)
-Lima = cbind(Lima$exp_grp, t(Lima$data))
-saveRDS(Lima,"df_Lima.rds")
