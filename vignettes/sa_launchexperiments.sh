4650f13bd4a643132d8ce78c04519454c69983b5
Florent Chuffart <florent.chuffart@gmail.com>
2022-07-20 11:15:51 +0200

    as sanke make fashion

--- vignettes/sa_launchexperiments.sh
+++ vignettes/sa_launchexperiments.sh
@@ -1,12 +1,42 @@
-#!/bin/bash
-# Prepare df for numerical exploration
-echo "source('sa_preparedf.R')" | Rscript -
+import os 
+
+with open("ourgses.txt") as f:
+    gses = f.read().splitlines() 
+
+curdir = os.getcwd()
+results = [f"{curdir}/results_{gse}.rds" for gse in gses]
 
 
-# for GSE in `cat ourgses.txt`
-# do
-#   RCODE="MODESA=TRUE ; gse='${GSE}' ; source('sa_callpipeline.R')"
-#   echo ${RCODE}
-#   echo ${RCODE} | Rscript -
-# done
+    
 
+localrules: target
+
+rule target:
+    threads: 1
+    message: "-- Rule target completed. --"
+    input: 
+      results
+    shell:"""
+pwd
+          """
+
+
+
+
+rule sa_callpipeline:
+    input: 
+      df="{prefix}/df_{gse}.rds",
+      rmd_script="{prefix}/sa_callpipeline.R",      
+      rmd_script_model="{prefix}/04_model.Rmd",      
+    output: 
+      rds = "{prefix}/results_{gse}.rds"      ,           
+    threads: 1
+    shell:"""
+# export PATH="/summer/epistorage/opt/bin:$PATH"
+# export PATH="/summer/epistorage/miniconda3/bin:$PATH"
+cd {wildcards.prefix}
+
+RCODE="MODESA=TRUE ; gse='{wildcards.gse}' ; source('sa_callpipeline.R')"
+echo $RCODE 
+echo $RCODE | Rscript -
+"""
