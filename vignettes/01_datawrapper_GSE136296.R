39ee454957e823affd3b982460235042966377bc
Florent Chuffart <florent.chuffart@gmail.com>
2022-10-29 01:22:12 +0200

    Adding GSE136296 built

---
+++ vignettes/01_datawrapper_GSE136296.R
@@ -0,0 +1,54 @@
+current_dir = getwd()
+setwd(paste0("~/projects/datashare/", gse))
+if (!file.exists("GSE136296_eegoguevara.2019.08.23.data.beta.pdet.csv.gz")) {
+  cmd = "wget"
+  args = "https://ftp.ncbi.nlm.nih.gov/geo/series/GSE136nnn/GSE136296/suppl/GSE136296_eegoguevara.2019.08.23.data.beta.pdet.csv.gz"
+  print(paste(cmd, args))
+  system2(cmd, args)
+}
+setwd(current_dir)
+if (! exists("mread.tablegz")) {
+  mread.tablegz = memoise::memoise(function(matrix_file, ...) {
+    read.table(gzfile(matrix_file), ...)
+  }, cache = cachem::cache_mem(max_size = 10*1024 * 1024^2))  
+}
+foo = mread.tablegz(paste0("~/projects/datashare/", gse, "/GSE136296_eegoguevara.2019.08.23.data.beta.pdet.csv.gz"), nrow=100, header=TRUE, row.names=1, sep=",")
+head(foo[,1:10])
+dim(foo)
+
+foo = mread.tablegz(paste0("~/projects/datashare/", gse, "/GSE136296_eegoguevara.2019.08.23.data.beta.pdet.csv.gz"), header=TRUE, row.names=1, sep=",")
+
+head(foo[,1:10])
+dim(foo)
+
+pdf()
+layout(matrix(1:2, 1), respect=TRUE)
+plot(density(foo[,1], na.rm=TRUE))
+plot(density(foo[,2], na.rm=TRUE))
+dev.off()
+foo = foo[,-grep("detP", colnames(foo), fixed=TRUE)]
+
+head(foo[,1:10])
+dim(foo)
+
+tmp_data = foo
+colnames(tmp_data)
+
+s$exp_grp$key = paste0("X", gsub("\\*|-", ".", s$exp_grp[,1]), "_beta")
+s$exp_grp$key
+colnames(tmp_data) %in% s$exp_grp$key
+tmp_data = tmp_data[,s$exp_grp$key]
+colnames(tmp_data) == s$exp_grp$key
+colnames(tmp_data) = rownames(s$exp_grp)
+colnames(tmp_data)
+tmp_data = as.matrix(tmp_data)
+
+
+s$data = tmp_data
+
+
+
+
+
+
+
---
+++ vignettes/01_expgrpwrapper_GSE136296.R
@@ -0,0 +1,25 @@
+s$exp_grp$"Sex:ch1"  
+s$exp_grp$gender = as.factor(as.character(s$exp_grp$"Sex:ch1"))  
+sort(s$exp_grp$gender)
+
+dim(s$exp_grp)
+s$exp_grp$gender01 = as.numeric(s$exp_grp$gender)
+library(EpiDISH)
+betaData.m = s$data
+dim(s$data)
+BloodFrac.m <- epidish(betaData.m, ref.m = centDHSbloodDMC.m, method = "RPC")$estF
+head(BloodFrac.m)
+s$exp_grp = cbind(s$exp_grp, BloodFrac.m)
+
+
+
+s$data = s$data[,rownames(s$exp_grp)] 
+
+
+# remove probes on chrX and chrY
+head(s$platform[,1:6]) 
+table(s$platform[,1]) 
+s$platform = s$platform[!s$platform[,1] %in% c("chrX", "chrY"),]
+table(s$platform[,1]) 
+
+s$data = s$data[rownames(s$platform),]
