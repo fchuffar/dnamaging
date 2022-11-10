be999ed77e824d07e66ba7b4f529f62baccc3e49
FOXE38 <fabienjossaud@gmail.com>
2022-11-10 09:02:06 +0100

    Modification launch_build

--- vignettes/launch_build_fast.py
+++ vignettes/launch_build_fast.py
@@ -1,4 +1,5 @@
 import os 
+import os.path
 
 with open("gse_build_fast.txt") as f:
     gses = f.read().splitlines() 
@@ -7,8 +8,16 @@ curdir = os.getcwd()
 df = [f"{curdir}/../../datashare/{gse}/df_{gse}.rds" for gse in gses]
 fpip = [f"{curdir}/00_fullpipeline1_{gse}.html" for gse in gses]
 
-    
-
+for gse in gses:
+    dw_exists = os.path.exists(f"{curdir}/01_datawrapper_{gse}.R")
+    exp_exists = os.path.exists(f"{curdir}/01_expgrpwrapper_{gse}.R")
+    if not dw_exists:
+        fichier = open(f"{curdir}/01_datawrapper_{gse}.R", "x")
+        fichier.close()
+    if not exp_exists:
+        fichier = open(f"{curdir}/01_expgrpwrapper_{gse}.R", "x")
+        fichier.close()
+        
 localrules: target
 
 rule target:
@@ -27,6 +36,8 @@ pwd
 rule build_gse:
     input: 
       rmd_build="{prefix}/01_build_study_generic.Rmd",
+      r_dw="{prefix}/01_datawrapper_{gse}.R",
+      r_exp="{prefix}/01_expgrpwrapper_{gse}.R",
     output: 
       study_rds = "{prefix}/../../datashare/{gse}/study_{gse}.rds",
       df_rds = "{prefix}/../../datashare/{gse}/df_{gse}.rds"    ,           
@@ -37,8 +48,17 @@ export PATH="/summer/epistorage/opt/bin:$PATH"
 export PATH="/summer/epistorage/miniconda3/envs/R3.6.1_env/bin:$PATH"
 cd {wildcards.prefix}
 
+rm -Rf /tmp/wd_{wildcards.gse}
+mkdir -p /tmp/wd_{wildcards.gse}
+cd /tmp/wd_{wildcards.gse}
+cp {wildcards.prefix}/01_build_study_generic.Rmd {wildcards.prefix}/01_datawrapper_{wildcards.gse}.R {wildcards.prefix}/01_expgrpwrapper_{wildcards.gse}.R .
+
 RCODE="Sys.setenv('VROOM_CONNECTION_SIZE'=10000000);gse='{wildcards.gse}' ; print(paste0('#### ',gse,' ####')); rmarkdown::render('01_build_study_generic.Rmd',output_file=paste0('01_build_study_',gse,'.html')); print(paste0(gse,' constructed'));"
 echo $RCODE | Rscript - 2>&1 > 01_build_study_{wildcards.gse}.Rout
+
+cp 01_build_study_{wildcards.gse}.Rout 01_build_study_{wildcards.gse}.html {wildcards.prefix}/.
+cd {wildcards.prefix}
+rm -Rf /tmp/wd_{wildcards.gse}
 """
 
 rule launch_pipeline:
@@ -54,6 +74,17 @@ export PATH="/summer/epistorage/opt/bin:$PATH"
 export PATH="/summer/epistorage/miniconda3/envs/R3.6.1_env/bin:$PATH"
 cd {wildcards.prefix}
 
+rm -Rf /tmp/wd_{wildcards.gse}
+mkdir -p /tmp/wd_{wildcards.gse}
+cd /tmp/wd_{wildcards.gse}
+ln -s {wildcards.prefix}/df_{wildcards.gse}.rds
+ln -s {wildcards.prefix}/litterature_models.rds
+cp {wildcards.prefix}/*.Rmd {wildcards.prefix}/*.R .
+
 RCODE="gse='{wildcards.gse}' ; rmarkdown::render('00_fullpipeline1.Rmd',output_file=paste0('00_fullpipeline1_',gse,'.html'));print(paste0(gse,'launched'));"
 echo $RCODE | Rscript - 2>&1 > 00_fullpipeline1_{wildcards.gse}.Rout
+
+cp 00_fullpipeline1_{wildcards.gse}.html 00_fullpipeline1_{wildcards.gse}.Rout {wildcards.prefix}/.
+cd {wildcards.prefix}
+rm -Rf /tmp/wd_{wildcards.gse}
 """
