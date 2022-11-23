caea0379530ec143b798af55f52e5e3af0bf2695
Florent Chuffart <florent.chuffart@gmail.com>
2022-11-23 17:17:27 +0100

    Update wf

--- vignettes/launch_build_fast.py
+++ vignettes/launch_build_fast.py
@@ -1,23 +1,16 @@
 import os 
 import os.path
 
-with open("gse_build_fast.txt") as f:
-    gses = f.read().splitlines() 
-
 curdir = os.getcwd()
+
+gses = [
+  "GSE41037"  ,
+  "GSE136296" ,
+  "GSE97362"
+]
 df = [f"{curdir}/../../datashare/{gse}/df_{gse}.rds" for gse in gses]
 fpip = [f"{curdir}/00_fullpipeline1_{gse}.html" for gse in gses]
 
-# for gse in gses:
-#     dw_exists = os.path.exists(f"{curdir}/01_datawrapper_{gse}.R")
-#     exp_exists = os.path.exists(f"{curdir}/01_expgrpwrapper_{gse}.R")
-#     if not dw_exists:
-#         fichier = open(f"{curdir}/01_datawrapper_{gse}.R", "x")
-#         fichier.close()
-#     if not exp_exists:
-#         fichier = open(f"{curdir}/01_expgrpwrapper_{gse}.R", "x")
-#         fichier.close()
-        
 localrules: target create_empty_wrapper
 
 rule target:
@@ -32,17 +25,26 @@ pwd
 
 
 
-rule create_empty_wrapper:
+rule create_empty_expgrpwrapper:
     input: 
       rmd_build="{prefix}/01_build_study_generic.Rmd",
     output: 
-      r_datawrapper="{prefix}/01_datawrapper_{gse}.R",
       r_expgrpwrapper="{prefix}/01_expgrpwrapper_{gse}.R",
     threads: 1
     shell:"""
 cd {wildcards.prefix}
-touch {wildcards.r_datawrapper}
 touch {wildcards.r_expgrpwrapper}
+"""
+
+rule create_empty_datawrapper:
+    input: 
+      rmd_build="{prefix}/01_build_study_generic.Rmd",
+    output: 
+      r_datawrapper="{prefix}/01_datawrapper_{gse}.R",
+    threads: 1
+    shell:"""
+cd {wildcards.prefix}
+touch {wildcards.r_datawrapper}
 """
 
         
@@ -51,8 +53,8 @@ touch {wildcards.r_expgrpwrapper}
 rule build_gse:
     input: 
       rmd_build="{prefix}/01_build_study_generic.Rmd",
-      r_dw="{prefix}/01_datawrapper_{gse}.R",
-      r_exp="{prefix}/01_expgrpwrapper_{gse}.R",
+      r_datawrapper="{prefix}/01_datawrapper_{gse}.R",
+      r_expgrpwrapper="{prefix}/01_expgrpwrapper_{gse}.R",
     output: 
       study_rds = "{prefix}/../../datashare/{gse}/study_{gse}.rds",
       df_rds = "{prefix}/../../datashare/{gse}/df_{gse}.rds"    ,           
@@ -66,9 +68,9 @@ cd {wildcards.prefix}
 rm -Rf /tmp/wd_{wildcards.gse}
 mkdir -p /tmp/wd_{wildcards.gse}
 cd /tmp/wd_{wildcards.gse}
-cp {wildcards.prefix}/01_build_study_generic.Rmd {wildcards.prefix}/01_datawrapper_{wildcards.gse}.R {wildcards.prefix}/01_expgrpwrapper_{wildcards.gse}.R .
+cp {input.rmd_build} {input.r_datawrapper} {input.r_expgrpwrapper} .
 
-RCODE="Sys.setenv('VROOM_CONNECTION_SIZE'=10000000);gse='{wildcards.gse}' ; print(paste0('#### ',gse,' ####')); rmarkdown::render('01_build_study_generic.Rmd',output_file=paste0('01_build_study_',gse,'.html')); print(paste0(gse,' constructed'));"
+RCODE="gse='{wildcards.gse}' ; print(paste0('#### ',gse,' ####')); rmarkdown::render('01_build_study_generic.Rmd',output_file=paste0('01_build_study_',gse,'.html')); print(paste0(gse,' constructed'));"
 echo $RCODE | Rscript - 2>&1 > 01_build_study_{wildcards.gse}.Rout
 
 cp 01_build_study_{wildcards.gse}.Rout 01_build_study_{wildcards.gse}.html {wildcards.prefix}/.
