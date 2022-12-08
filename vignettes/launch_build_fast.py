c667e7e58b5ae0a44997d06f08a408136d75ed73
Florent Chuffart <florent.chuffart@gmail.com>
2022-12-08 15:29:16 +0100

    fixing report

--- vignettes/launch_build_fast.py
+++ vignettes/launch_build_fast.py
@@ -10,8 +10,8 @@ gses = [
   "GSE42861"
 ]
 
-df = [f"{curdir}/../../datashare/{gse}/df_{gse}.rds" for gse in gses]
-fpip = [f"{curdir}/00_fullpipeline1_{gse}.html" for gse in gses]
+info_models = [f"{curdir}/info_model_{gse}.rds" for gse in gses]
+info_builds = [f"{curdir}/info_build_{gse}.rds" for gse in gses]
 
 localrules: target create_empty_expgrpwrapper create_empty_datawrapper info_gse
 
@@ -19,11 +19,11 @@ rule target:
     threads: 1
     message: "-- Rule target completed. --"
     input: 
-      df,
-      fpip,
+      info_builds,
+      info_models,
     shell:"""
 pwd
-RCODE="source(data_info.R)"
+RCODE="source('data_info.R')"
 echo $RCODE | Rscript - 2>&1 > data_info.Rout
           """
 
@@ -55,9 +55,10 @@ rule build_gse:
       r_datawrapper="{prefix}/01_datawrapper_{gse}.R",
       r_expgrpwrapper="{prefix}/01_expgrpwrapper_{gse}.R",
     output: 
-      study_rds = "{prefix}/../../datashare/{gse}/study_{gse}.rds",
-      df_rds = "{prefix}/../../datashare/{gse}/df_{gse}.rds"    ,           
-      html = "{prefix}/01_build_study_{gse}.html"      ,           
+      study_rds =   "{prefix}/../../datashare/{gse}/study_{gse}.rds",
+      df_rds =      "{prefix}/../../datashare/{gse}/df_{gse}.rds"    ,           
+      html =        "{prefix}/01_build_study_{gse}.html"      ,           
+      info_build =  "{prefix}/info_build_{gse}.rds"   ,
     threads: 8
     shell:"""
 export PATH="/summer/epistorage/opt/bin:$PATH"
@@ -72,18 +73,20 @@ cp {input.rmd_build} {input.r_datawrapper} {input.r_expgrpwrapper} .
 RCODE="gse='{wildcards.gse}' ; print(paste0('#### ',gse,' ####')); rmarkdown::render('01_build_study_generic.Rmd',output_file=paste0('01_build_study_',gse,'.html')); print(paste0(gse,' constructed'));"
 echo $RCODE | Rscript - 2>&1 > 01_build_study_{wildcards.gse}.Rout
 
-cp 01_build_study_{wildcards.gse}.Rout 01_build_study_{wildcards.gse}.html {wildcards.prefix}/.
+cp 01_build_study_{wildcards.gse}.Rout info_build_{wildcards.gse}.rds 01_build_study_{wildcards.gse}.html {wildcards.prefix}/.
 cd {wildcards.prefix}
 rm -Rf /tmp/wd_{wildcards.gse}
 """
 
 rule launch_pipeline:
     input: 
-      df="{prefix}/../../datashare/{gse}/df_{gse}.rds",
+      df=os.path.expanduser("~/projects/datashare/{gse}/df_{gse}.rds"),
       rmd_script="{prefix}/00_fullpipeline1.Rmd",      
       rmd_script_model="{prefix}/04_model.Rmd",      
     output:           
-      html = "{prefix}/00_fullpipeline1_{gse}.html"      ,           
+      html =       "{prefix}/00_fullpipeline1_{gse}.html"      ,           
+      info_desc  = "{prefix}/info_desc_{gse}.rds"      ,           
+      info_model = "{prefix}/info_model_{gse}.rds"      ,           
     threads: 32
     shell:"""
 export PATH="/summer/epistorage/opt/bin:$PATH"
@@ -100,7 +103,7 @@ cp {wildcards.prefix}/*.Rmd {wildcards.prefix}/*.R .
 RCODE="gse='{wildcards.gse}' ; nb_core=6; rmarkdown::render('00_fullpipeline1.Rmd',output_file=paste0('00_fullpipeline1_',gse,'.html'));print(paste0(gse,'launched'));"
 echo $RCODE | Rscript - 2>&1 > 00_fullpipeline1_{wildcards.gse}.Rout
 
-cp 00_fullpipeline1_{wildcards.gse}.html 00_fullpipeline1_{wildcards.gse}.Rout {wildcards.prefix}/.
+cp 00_fullpipeline1_{wildcards.gse}.html 00_fullpipeline1_{wildcards.gse}.Rout info_desc_{wildcards.gse}.rds info_model_{wildcards.gse}.rds {wildcards.prefix}/. 
 cd {wildcards.prefix}
 rm -Rf /tmp/wd_{wildcards.gse}
 """
