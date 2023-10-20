4ad24adc657a23216616e08d9067d8b4d33b9625
Florent Chuffart <florent.chuffart@gmail.com>
2023-10-20 09:05:26 +0200

    Migrate df approach to study approach

--- misc/generate_data.R
+++ misc/generate_data.R
@@ -1,18 +1,5 @@
-source("../R/functions.R")
-df_filename = paste0("../misc/df_", "GSE41037", ".rds")
-df = mreadRDS(df_filename)
-
-idx_samples = rownames(df)
-markers_start = grep("cg",colnames(df))[1]
-idx_clinicals = colnames(df)[1:(markers_start-1)]
-idx_cpg = colnames(df)[markers_start:ncol(df)]
-
-
-set.seed(1)
-df_dnamaging = df[, c("gender", "age", sample(idx_cpg, 3000))]
-save(df_dnamaging, file='~/projects/dnamaging/data/df_dnamaging.RData' , compress='xz')
-# DEPRECATED, see `r gse = 'GSE40279' ; rmarkdown::render("04_model.Rmd")`
-
+study_dnamaging = readRDS("~/projects/ewascombpr/vignettes/study_GSE42861_modelcalllm_meth~age_ewas2000_nn1000.rds")
+save(study_dnamaging, file='~/projects/dnamaging/data/study_dnamaging.RData' , compress='xz')
 
 litterature_models = mreadRDS("../vignettes/litterature_models.rds")
 save(litterature_models, file='~/projects/dnamaging/data/litterature_models.RData' , compress='xz')
