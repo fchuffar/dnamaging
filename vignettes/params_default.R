c827d66a1f035cf622c6ee4fbc872d7d6a69e126
Florent Chuffart <florent.chuffart@gmail.com>
2023-11-24 18:09:50 +0100

    Fixing params params_GSE call in params_default.R

--- vignettes/params_default.R
+++ vignettes/params_default.R
@@ -8,7 +8,7 @@ n_boot = 500
 FILTER_EWAS_FOR_DMR_CANDIDATE = FALSE
 FILTER_SEXUAL_CHR = TRUE
 
-custom_params_filename = paste0("params_", strsplit(gse, "given")[[1]][1], ".R")
+custom_params_filename = paste0("params_", strsplit(gse, "given|_")[[1]][1], ".R")
 custom_params_filename
 if (file.exists(custom_params_filename)) source(custom_params_filename)
   
