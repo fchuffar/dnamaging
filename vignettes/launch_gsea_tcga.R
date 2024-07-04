5eb1f46abad5a3585f405b18718817de67b52d7f
jossaudf <fabienjossaud@gmail.com>
2024-07-04 11:55:55 +0200

    Update last versioning of GSEA pipeline

--- vignettes/launch_gsea_tcga.R
+++ vignettes/launch_gsea_tcga.R
@@ -11,33 +11,27 @@ data_list = c("CustGSE152710mds",
 "TCGA-KIRP",
 "TCGA-HNSC",
 "BRB",
-"GSE42861")
- 
-for (pred_gse in c("GSE147740")) {
+"GSE42861"
+)
+
+pred_list = c(#"GSE147740", 
+#"avmps", 
+#"combp",
+"CustGSE147740rr"
+)
+
+for (pred_gse in pred_list) {
 	for (data_type in data_list) {
 		rmarkdown::render("gsea_tcga.Rmd", output_file = paste0("gsea_",data_type,"_",pred_gse,".html")) 
 	}
+	rmarkdown::render("resume_gsea.Rmd", output_file = paste0("resume_gsea_",pred_gse,".html")) 
 }
 
-foo = sapply(paste0("info_disease_",data_list,"_GSE147740.rds"), function (f) {
-	readRDS(f)
-})
-foo = t(foo)
-foo = data.frame(lapply(data.frame(foo, stringsAsFactors=FALSE), unlist), stringsAsFactors=FALSE)
-rownames(foo) = foo$disease
 
-layout(matrix(1:3,1), respect=TRUE)
-plot(-log10(foo$pv_up),-log10(foo$pv_dw), xlim = c(0,3.5), ylim = c(0,3.5), col = 0)
-text(-log10(foo$pv_up), -log10(foo$pv_dw), foo$disease)
-abline(v= -log10(0.05), lty = 3)
-abline(h= -log10(0.05), lty = 3)
 
-r2 = foo$r2_mean
-names(r2) = foo$disease
-r2 = sort(r2, decreasing = TRUE)
-barplot(r2, las = 2, ylim = c(0,1))
 
-ind = foo$n_ind
-names(ind) = foo$disease
-ind = sort(ind, decreasing = TRUE)
-barplot(ind, las = 2)
+
+
+
+
+
